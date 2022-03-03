49c74874e2c3e50c98c34a6a4e8cb8dcfa99e302
tjufc <fc2012auto@tju.edu.cn>
2022-03-04 00:35:36 +0800

    add lua-resty-waf

---
+++ lua-resty-waf/lib/resty/waf/phase.lua
@@ -0,0 +1,22 @@
+--[[
+phase.lua - 阶段
+resty-waf一共作用于4个阶段：
+	access
+	header_filter
+	body_filter
+	log
+]]
+local _M = {}
+
+local base = require "resty.waf.base"
+local util = require "resty.waf.util"
+
+_M.version = base.version
+
+_M.phases = { access = 1, header_filter = 2, body_filter = 3, log = 4 }
+
+function _M.is_valid_phase(phase)
+	return util.table_has_key(phase, _M.phases)
+end
+
+return _M
